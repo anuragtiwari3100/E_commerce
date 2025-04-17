afe24c125889ed026f3a2064c6102814e1e32de5
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-04-17 22:28:08 +0530

    Add HomeController & ApiResponse, remove application.properties from tracking

--- Ecommerce_Multivender/src/main/java/com/zosh/controller/HomeController.java
+++ Ecommerce_Multivender/src/main/java/com/zosh/controller/HomeController.java
@@ -1,4 +1,16 @@
 package com.zosh.controller;
 
+import com.zosh.response.ApiResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
+
+    @GetMapping
+    public ApiResponse HomeControllerHandler(){
+        ApiResponse  apiResponse = new ApiResponse();
+        apiResponse.setMessage("welcome to ecommerce multi vendor System") ;
+         return apiResponse ;
+    }
 }
